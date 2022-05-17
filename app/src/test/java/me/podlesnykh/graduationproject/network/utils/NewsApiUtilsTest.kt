f227f118563c115b577d20c5bd69703793b6d385
desttruptor <41233120+desttruptor@users.noreply.github.com>
2022-05-17 19:39:27 +0300

    added unit tests to utils class

---
+++ app/src/test/java/me/podlesnykh/graduationproject/network/utils/NewsApiUtilsTest.kt
@@ -0,0 +1,39 @@
+package me.podlesnykh.graduationproject.network.utils
+
+import com.google.common.truth.Truth
+import me.podlesnykh.graduationproject.network.utils.NewsApiUtils.encodeUrl
+import me.podlesnykh.graduationproject.network.utils.NewsApiUtils.searchIn
+import org.junit.Test
+
+/**
+ * Tests for [NewsApiUtils]
+ */
+class NewsApiUtilsTest {
+    @Test
+    fun encodeUrlTest() {
+        val stringToEncode = "This, is. A string! to EnCodE"
+        val expected = "This%2C+is.+A+string%21+to+EnCodE"
+        Truth.assertThat(stringToEncode.encodeUrl()).isEqualTo(expected)
+    }
+
+    @Test
+    fun searchInTest_none() {
+        Truth.assertThat(
+            searchIn(title = false, description = false, content = false)
+        ).isEqualTo("")
+    }
+
+    @Test
+    fun searchInTest_TitleDescription() {
+        Truth.assertThat(
+            searchIn(content = false)
+        ).isEqualTo("title,description")
+    }
+
+    @Test
+    fun searchInTest_All() {
+        Truth.assertThat(
+            searchIn()
+        ).isEqualTo("title,description,content")
+    }
+}
